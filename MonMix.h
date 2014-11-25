6a62ac3166e4405b12db053b0ec39d00c0c079ce
André pierre <temps.jo@gmail.com>
2014-11-25 23:37:57 +0100

    Create MonMix.h

---
+++ MonMix.h
@@ -0,0 +1,87 @@
+#ifndef DEF_MONMIX
+#define DEF_MONMIX
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <iostream>
+#include <iostream>
+#include <QObject>
+using namespace std;
+class MonMix : public QWidget
+{
+Q_OBJECT
+
+public:
+MonMix();
+private:
+QPushButton *aa_bouton;
+QPushButton *ba_bouton;
+QPushButton *bb_bouton;
+QPushButton *bc_bouton;
+QPushButton *bd_bouton;
+QPushButton *be_bouton;
+QPushButton *bf_bouton;
+QPushButton *bg_bouton;
+QPushButton *bh_bouton;
+QPushButton *bi_bouton;
+QPushButton *bj_bouton;
+QPushButton *bk_bouton;
+QPushButton *bl_bouton;
+QPushButton *bm_bouton;
+QPushButton *bn_bouton;
+QPushButton *bo_bouton;
+QPushButton *bp_bouton;
+QPushButton *bq_bouton;
+QPushButton *br_bouton;
+QPushButton *bs_bouton;
+QPushButton *bt_bouton;
+QPushButton *bu_bouton;
+QPushButton *bv_bouton;
+QPushButton *bw_bouton;
+QPushButton *bx_bouton;
+QPushButton *by_bouton;
+QPushButton *bz_bouton;
+QPushButton *ca_bouton;
+QPushButton *cb_bouton;
+QPushButton *cc_bouton;
+QPushButton *cd_bouton;
+QPushButton *ce_bouton;
+QPushButton *cf_bouton;
+
+public slots:
+void ouvrirCompose1();
+void ouvrirCompose2();
+void ouvrirCompose3();
+void ouvrirCompose4();
+void ouvrirCompose5();
+void ouvrirCompose6();
+void ouvrirCompose7();
+void ouvrirCompose8();
+void ouvrirCompose9();
+void ouvrirCompose10();
+void ouvrirCompose11();
+void ouvrirCompose12();
+void ouvrirCompose13();
+void ouvrirCompose14();
+void ouvrirCompose15();
+void ouvrirCompose16();
+void ouvrirMix1();
+void ouvrirMix2();
+void ouvrirMix3();
+void ouvrirMix4();
+void ouvrirMix5();
+void ouvrirMix6();
+void ouvrirMix7();
+void ouvrirMix8();
+void ouvrirMix9();
+void ouvrirMix10();
+void ouvrirMix11();
+void ouvrirMix12();
+void ouvrirMix13();
+void ouvrirMix14();
+void ouvrirMix15();
+void ouvrirMix16();
+
+
+};
+#endif // MIX_H
