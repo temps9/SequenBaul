071cc280606bcda85165244f0e156bbf979ee607
André pierre <temps.jo@gmail.com>
2014-11-25 23:49:16 +0100

    Create AdnForme3.h

---
+++ AdnForme3.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme3_h_
+#define _AdnForme3_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo3(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme3_h_
