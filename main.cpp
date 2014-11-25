926e82217e2e0b87a9adce624e5b063acc1aeaed
André pierre <temps.jo@gmail.com>
2014-11-25 23:35:21 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <QApplication>
+#include "MonMix.h"
+#include <iostream>
+using namespace std;
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <QThread>
+#include <QObject>
+#include <QDesktopWidget>
+
+
+/* Imaginer et creer par Abadie joris*/
+/* en utilisant un concept de Andre pierre jocelyn*/
+/*Cette oeuvre est protégée et n'est divulguée et réservé que pour utilisation sous linux dans les conditions Creative common ci-dessous*/
+/* Cette oeuvre est mise à disposition selon les termes de la Licence Creative Commons Paternité - Pas d'Utilisation Commerciale - Partage des Conditions Initiales à l'Identique 2.0 France. */
+/*Aucune garantie n'est donner sur les effets de cet executable*/
+
+#include <iostream>
+using namespace std;
+/**dépendance sox */
+#include <string.h>
+#include <dirent.h>
+#include "math.h"
+
+int main(int argc, char *argv[])
+{
+
+    char donne[15];
+    char donne1[15];
+      for(int i=1; i<17; i++)
+       {
+  snprintf(donne, 15,"fichier%d",  i);
+    snprintf(donne1, 15,"piste%d.txt",  i);
+   FILE* donne = NULL;
+   donne = fopen (donne1, "w+");
+    if (donne != NULL)
+    {
+          fprintf(donne, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d \n", 41, 1, 133, 130, 50, 50, 12, 12, 0, 0, 1, 7, 50, 5, 10, 2);
+        fclose(donne);
+    }
+        }
+
+    QApplication app(argc, argv);
+    MonMix fenetre;
+    fenetre.show();
+
+    return app.exec();
+
+}
