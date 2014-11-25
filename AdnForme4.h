a56bfc07f08e0069a86b9993e3c3048724a03b4d
André pierre <temps.jo@gmail.com>
2014-11-25 23:51:50 +0100

    Create AdnForme4.h

---
+++ AdnForme4.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme4_h_
+#define _AdnForme4_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo4(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _AdnForme4_h_
