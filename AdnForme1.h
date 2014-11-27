4b1e47f0af6b3d69c0cfd52462c05cee28554c45
André pierre <temps.jo@gmail.com>
2014-11-27 21:53:00 +0100

    Update AdnForme1.h

--- AdnForme1.h
+++ AdnForme1.h
@@ -3,10 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "jo.h"
-#include "fronts.h"
 
-void genereFichierJo1(char c);
-void conversionJoWav(char c, int n);
+void genereFichierJo1(int piste);
+void conversionJoWav();
 
 #endif // _AdnForme_h_
