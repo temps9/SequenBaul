4a0154374e7c66819d84db1eee9405b62a70f98e
André pierre <temps.jo@gmail.com>
2014-11-27 22:14:03 +0100

    Create AdnForme18.cpp

---
+++ AdnForme18.cpp
@@ -0,0 +1,199 @@
+#include "AdnForme18.h"
+
+// Génère les fichiers c.jo et c.joa
+void genereFichierJo18(int piste,int quelpiano2,int quelpiano3,int quelpiano4,int quelpiano5,int quelpiano6,int quelpiano7,int quelpiano8,int quelpiano9,int quelpiano10,int quelpiano11,int quelpiano12,int quelpiano13,int quelpiano14,int quelpiano15)
+{
+
+    int mesamplifront[2]; //son du n, la référence
+    int mesdurerfront[2]; //son du n, la référence
+    int mesvarampli[2]; //son du n, la référence
+    int mesvardurer[2]; //son du n, la référence
+    int madurer; //son du n, la référence
+    int maforceh;
+    int maforceb;
+    int montremolo;
+    int maforceplus;
+    int macombiendezonememoire;
+
+mesamplifront[0] = quelpiano2;
+mesamplifront[1] = quelpiano3;
+mesdurerfront[0] = quelpiano4;
+mesdurerfront[1] = quelpiano5;
+mesvarampli[0] = quelpiano6;
+mesvarampli[1] = quelpiano7;
+mesvardurer[0] = quelpiano8;
+mesvardurer[1] = quelpiano9;
+madurer = quelpiano10;
+maforceh = quelpiano11;
+maforceb = quelpiano12;
+montremolo = quelpiano13;
+maforceplus = quelpiano14;
+macombiendezonememoire = quelpiano15;
+
+  // Nom du fichier jo
+  char nomfichierjo[32];
+  snprintf(nomfichierjo, 32, "%d.jo", piste);
+  // Fichier jo
+  FILE* fichierjo;
+  if (!(fichierjo = fopen(nomfichierjo, "ab")))
+   {
+    exit(-1);
+   }
+// ici commence la zone de travail pour l'apprenant
+int tablex[250]= {120, 60, 40, 30, 24, 20, 17, 15, 13, 12, 11, 10, 9, 9, 8, 8, 7, 7, 6, 6, 6, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+int calculpossible = tablex[maforceh-1];
+int ajusteur1;
+int ajusteur2;
+
+// multiplie
+  for(int duree = madurer; duree>0; duree--)
+   {
+
+//monte
+
+    for(int laforcebbase = maforceh; laforcebbase > 0; laforcebbase--)
+     {
+
+         mesamplifront[1] -= calculpossible;
+         mesamplifront[0] += calculpossible;
+	
+	 // respect des limites
+      for(int i=0; i<2; i++)
+       {
+	if(mesamplifront[i]>239)
+	mesamplifront[i] = 239;
+
+	if(mesamplifront[i]<16)
+	mesamplifront[i] = 16;	
+	}
+	ajusteur1 = mesamplifront[0] - 2;
+	ajusteur2 = mesamplifront[1] + 2;
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[0], (char)mesdurerfront[0]);
+
+        fprintf(fichierjo,  "%c%c",    (char)ajusteur1, (char)mesdurerfront[0]);
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[0], (char)mesdurerfront[0]);
+
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[1], (char)mesdurerfront[1]);
+
+        fprintf(fichierjo,  "%c%c",    (char)ajusteur2, (char)mesdurerfront[1]);
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[1], (char)mesdurerfront[1]);
+
+
+
+     }
+//plateau
+
+int basculeDe1 = montremolo * 2;
+int basculeDe2 = basculeDe1 - 1;
+int basculeDe3 = basculeDe1 * 2 - 1;
+int basculeDe5 = 0;
+
+    for(int laforcebbase = maforceb; laforcebbase > 0; laforcebbase--)
+     {
+
+if(basculeDe5<basculeDe1)
+{
+mesdurerfront[0] += mesvardurer[0];
+mesamplifront[0] -= mesvarampli[0];
+ajusteur1 -= mesvarampli[0];
+mesdurerfront[1] += mesvardurer[1];
+mesamplifront[1] += mesvarampli[1];
+ajusteur2 += mesvarampli[1];
+}
+
+if(basculeDe5>basculeDe2)
+{
+mesdurerfront[0] -= mesvardurer[0];
+mesamplifront[0] += mesvarampli[0];
+ajusteur1 += mesvarampli[0];
+mesdurerfront[1] -= mesvardurer[1];
+mesamplifront[1] -= mesvarampli[1];
+ajusteur2 -= mesvarampli[1];
+}
+basculeDe5++;
+if(basculeDe5>basculeDe3)
+basculeDe5 = 0;
+
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[0], (char)mesdurerfront[0]);
+
+        fprintf(fichierjo,  "%c%c",    (char)ajusteur1, (char)mesdurerfront[0]);
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[0], (char)mesdurerfront[0]);
+
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[1], (char)mesdurerfront[1]);
+
+        fprintf(fichierjo,  "%c%c",    (char)ajusteur2, (char)mesdurerfront[1]);
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[1], (char)mesdurerfront[1]);
+
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[0], (char)mesdurerfront[1]);
+
+        fprintf(fichierjo,  "%c%c",    (char)ajusteur1, (char)mesdurerfront[1]);
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[0], (char)mesdurerfront[1]);
+
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[1], (char)mesdurerfront[0]);
+
+        fprintf(fichierjo,  "%c%c",    (char)ajusteur2, (char)mesdurerfront[0]);
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[1], (char)mesdurerfront[0]);
+
+     }
+
+
+
+//descend
+
+    for(int laforcebbase = maforceh; laforcebbase > 0; laforcebbase--)
+     {
+
+         mesamplifront[1] += calculpossible;
+         mesamplifront[0] -= calculpossible;
+	
+	 // respect des limites
+      for(int i=0; i<2; i++)
+       {
+	if(mesamplifront[i]>239)
+	mesamplifront[i] = 239;
+
+	if(mesamplifront[i]<16)
+	mesamplifront[i] = 16;	
+	}
+	ajusteur1 = mesamplifront[0] - 2;
+	ajusteur2 = mesamplifront[1] + 2;
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[0], (char)mesdurerfront[0]);
+
+        fprintf(fichierjo,  "%c%c",    (char)ajusteur1, (char)mesdurerfront[0]);
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[0], (char)mesdurerfront[0]);
+
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[1], (char)mesdurerfront[1]);
+
+        fprintf(fichierjo,  "%c%c",    (char)ajusteur2, (char)mesdurerfront[1]);
+
+        fprintf(fichierjo,  "%c%c",    (char)mesamplifront[1], (char)mesdurerfront[1]);
+
+
+     }
+
+
+
+
+// fin  multiplie
+   }
+ 
+  fclose(fichierjo);
+
+}
+
+// fin de la zone de travail de l'apprenant
