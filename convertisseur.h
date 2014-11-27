8bdb19c10447dec040a6af9a0fc7970151248684
André pierre <temps.jo@gmail.com>
2014-11-27 13:15:04 +0100

    Create convertisseur.h

---
+++ convertisseur.h
@@ -0,0 +1,51 @@
+#ifndef _convertisseur_h_
+#define _convertisseur_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void genereFichierJo(int piste);
+//void genereFichierJo1(int piste);
+//void genereFichierJo2(int piste);
+//void genereFichierJo3(int piste);
+//void genereFichierJo4(int piste);
+//void genereFichierJo5(int piste);
+//void genereFichierJo6(int piste);
+//void genereFichierJo7(int piste);
+//void genereFichierJo8(int piste);
+//void genereFichierJo9(int piste);
+//void genereFichierJo10(int piste);
+//void genereFichierJo11(int piste);
+//void genereFichierJo12(int piste);
+//void genereFichierJo13(int piste);
+//void genereFichierJo14(int piste);
+//void genereFichierJo15(int piste);
+//void genereFichierJo16(int piste);
+//void genereFichierJo17(int piste);
+//void genereFichierJo18(int piste);
+//void genereFichierJo19(int piste);
+//void genereFichierJo20(int piste);
+//void genereFichierJo21(int piste);
+//void genereFichierJo22(int piste);
+//void genereFichierJo23(int piste);
+//void genereFichierJo24(int piste);
+//void genereFichierJo25(int piste);
+//void genereFichierJo26(int piste);
+//void genereFichierJo27(int piste);
+//void genereFichierJo28(int piste);
+//void genereFichierJo29(int piste);
+//void genereFichierJo30(int piste);
+//void genereFichierJo31(int piste);
+//void genereFichierJo32(int piste);
+//void genereFichierJo33(int piste);
+//void genereFichierJo34(int piste);
+//void genereFichierJo35(int piste);
+//void genereFichierJo36(int piste);
+//void genereFichierJo37(int piste);
+//void genereFichierJo38(int piste);
+//void genereFichierJo39(int piste);
+//void genereFichierJo40(int piste);
+//void genereFichierJo41(int piste);
+void conversionJoWav(int piste);
+
+#endif // _convertisseur_h_
