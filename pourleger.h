a10f8ed3cb2e47770a4082fb0c8d3eba423ec1f7
André pierre <temps.jo@gmail.com>
2014-11-28 18:27:36 +0100

    Create pourleger.h

---
+++ pourleger.h
@@ -0,0 +1,53 @@
+#ifndef _pourleger_h_
+#define _pourleger_h_
+
+#include "AdnForme.h"
+#include "AdnForme1.h"
+#include "AdnForme2.h"
+#include "AdnForme3.h"
+#include "AdnForme4.h"
+#include "AdnForme5.h"
+#include "AdnForme6.h"
+#include "AdnForme7.h"
+#include "AdnForme8.h"
+#include "AdnForme9.h"
+#include "AdnForme10.h"
+#include "AdnForme11.h"
+#include "AdnForme12.h"
+#include "AdnForme13.h"
+#include "AdnForme14.h"
+#include "AdnForme15.h"
+#include "AdnForme16.h"
+#include "AdnForme17.h"
+#include "AdnForme18.h"
+#include "AdnForme19.h"
+#include "AdnForme20.h"
+#include "AdnForme21.h"
+#include "AdnForme22.h"
+#include "AdnForme23.h"
+#include "AdnForme24.h"
+#include "AdnForme25.h"
+#include "AdnForme26.h"
+#include "AdnForme27.h"
+#include "AdnForme28.h"
+#include "AdnForme29.h"
+#include "AdnForme30.h"
+#include "AdnForme31.h"
+#include "AdnForme32.h"
+#include "AdnForme33.h"
+#include "AdnForme34.h"
+#include "AdnForme35.h"
+#include "AdnForme36.h"
+#include "AdnForme37.h"
+#include "AdnForme38.h"
+#include "AdnForme39.h"
+#include "AdnForme40.h"
+#include "AdnForme41.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void genereFichierJo(int piste);
+void monMixleger(int piste);
+void conversionJoWav(int piste);
+
+#endif // _pourleger_h_
