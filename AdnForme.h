930b64dacd87ad399d171a02d35fde2b2da8a11f
André pierre <temps.jo@gmail.com>
2014-11-28 17:58:18 +0100

    Update AdnForme.h

--- AdnForme.h
+++ AdnForme.h
@@ -1,10 +1,7 @@
 #ifndef _AdnForme_h_
 #define _AdnForme_h_
-
 #include <stdio.h>
 #include <stdlib.h>
-
-void genereFichierJo(int piste);
+void genereFichierJo(int piste,int quelpiano2,int quelpiano3,int quelpiano4,int quelpiano5,int quelpiano6,int quelpiano7,int quelpiano8,int quelpiano9,int quelpiano10,int quelpiano11,int quelpiano12,int quelpiano13,int quelpiano14,int quelpiano15);
 void conversionJoWav();
-
 #endif // _AdnForme_h_
