b01d5d7b93eb18753c8afb42f15642de2245cffa
André pierre <temps.jo@gmail.com>
2014-11-28 18:11:10 +0100

    Update AdnForme21.h

--- AdnForme21.h
+++ AdnForme21.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void genereFichierJo21(int piste);
+void genereFichierJo21(int piste,int quelpiano2,int quelpiano3,int quelpiano4,int quelpiano5,int quelpiano6,int quelpiano7,int quelpiano8,int quelpiano9,int quelpiano10,int quelpiano11,int quelpiano12,int quelpiano13,int quelpiano14,int quelpiano15);
 void conversionJoWav();
 
 #endif // _AdnForme21_h_
