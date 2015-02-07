a882356bdd09b5148dbe5aab669c4a9fe61238a6
André pierre <temps.jo@gmail.com>
2015-02-07 12:16:01 +0100

    Create AdnForme45.h

---
+++ AdnForme45.h
@@ -0,0 +1,7 @@
+#ifndef _AdnForme45_h_
+#define _AdnForme45_h_
+#include <stdio.h>
+#include <stdlib.h>
+void genereFichierJo45(int piste,int quelpiano2,int quelpiano3,int quelpiano4,int quelpiano5,int quelpiano6,int quelpiano7,int quelpiano8,int quelpiano9,int quelpiano10,int quelpiano11,int quelpiano12,int quelpiano13,int quelpiano14,int quelpiano15);
+void conversionJoWav();
+#endif // _AdnForme45_h_
