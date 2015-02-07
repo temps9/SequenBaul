d562d56757f649743dbffa60e8d6780c52517193
André pierre <temps.jo@gmail.com>
2015-02-07 11:51:23 +0100

    Create AdnForme65.h

---
+++ AdnForme65.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme65_h_
+#define _AdnForme65_h_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "consonnes.h"
+
+void genereFichierJo65(int piste,int quelpiano2,int quelpiano3,int quelpiano4,int quelpiano5,int quelpiano6,int quelpiano7,int quelpiano8,int quelpiano9,int quelpiano10,int quelpiano11,int quelpiano12,int quelpiano13,int quelpiano14,int quelpiano15);
+void conversionJoWav();
+
+#endif // _AdnForme65_h_
