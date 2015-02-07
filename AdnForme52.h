d7827252f72737189006e6edf76173fc46a0ab54
André pierre <temps.jo@gmail.com>
2015-02-07 12:07:45 +0100

    Create AdnForme52.h

---
+++ AdnForme52.h
@@ -0,0 +1,10 @@
+#ifndef _AdnForme52_h_
+#define _AdnForme52_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void genereFichierJo52(int piste,int quelpiano2,int quelpiano3,int quelpiano4,int quelpiano5,int quelpiano6,int quelpiano7,int quelpiano8,int quelpiano9,int quelpiano10,int quelpiano11,int quelpiano12,int quelpiano13,int quelpiano14,int quelpiano15);
+void conversionJoWav();
+
+#endif // _AdnForme52_h_
