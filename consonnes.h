f3bdca5c2b45522aef260d7cf2a751a55d684224
André pierre <temps.jo@gmail.com>
2015-02-07 11:37:27 +0100

    Create consonnes.h

---
+++ consonnes.h
@@ -0,0 +1,10 @@
+#ifndef _consonnes_h_
+#define _consonnes_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void choixconsonne(int piste, int quelpiano11, int quelpiano12, int quelpiano13, int quelpiano14, int quelpiano15);
+
+
+#endif // _consonnes_h_
