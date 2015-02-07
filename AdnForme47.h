d9bea602dab678c2284ba14429fbfb63e5231a03
André pierre <temps.jo@gmail.com>
2015-02-07 12:13:39 +0100

    Create AdnForme47.h

---
+++ AdnForme47.h
@@ -0,0 +1,10 @@
+#ifndef _AdnForme47_h_
+#define _AdnForme47_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void genereFichierJo47(int piste,int quelpiano2,int quelpiano3,int quelpiano4,int quelpiano5,int quelpiano6,int quelpiano7,int quelpiano8,int quelpiano9,int quelpiano10,int quelpiano11,int quelpiano12,int quelpiano13,int quelpiano14,int quelpiano15);
+void conversionJoWav();
+
+#endif // _AdnForme47_h_
