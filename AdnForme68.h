0d550e4cb7aa785e2021354b03ebd221d094b230
André pierre <temps.jo@gmail.com>
2015-02-07 11:47:52 +0100

    Create AdnForme68.h

---
+++ AdnForme68.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme68_h_
+#define _AdnForme68_h_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "consonnes.h"
+
+void genereFichierJo68(int piste,int quelpiano2,int quelpiano3,int quelpiano4,int quelpiano5,int quelpiano6,int quelpiano7,int quelpiano8,int quelpiano9,int quelpiano10,int quelpiano11,int quelpiano12,int quelpiano13,int quelpiano14,int quelpiano15);
+void conversionJoWav();
+
+#endif // _AdnForme68_h_
