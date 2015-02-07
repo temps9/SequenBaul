5751efc40a12e9172616ade48f2fb6dae00ceff5
André pierre <temps.jo@gmail.com>
2015-02-07 11:50:16 +0100

    Create AdnForme66.h

---
+++ AdnForme66.h
@@ -0,0 +1,12 @@
+#ifndef _AdnForme66_h_
+#define _AdnForme66_h_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "consonnes.h"
+
+void genereFichierJo66(int piste,int quelpiano2,int quelpiano3,int quelpiano4,int quelpiano5,int quelpiano6,int quelpiano7,int quelpiano8,int quelpiano9,int quelpiano10,int quelpiano11,int quelpiano12,int quelpiano13,int quelpiano14,int quelpiano15);
+void conversionJoWav();
+
+#endif // _AdnForme66_h_
